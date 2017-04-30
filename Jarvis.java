0fd4d9046ae53300bbc4fa0ef785d2adb351e572
Roshini Jagadam <roshinijj@yahoo.com>
2017-04-30 17:44:57 -0400

    Update Jarvis.java

--- Jarvis.java
+++ Jarvis.java
@@ -9,17 +9,19 @@ Lights lights =new Lights();
 Door door = new door();
 Camera camera = new Camera();
 Spotify spotify = new Spotify();
+Thermostat thermostat = new Thermostat();
 Oven oven = new Oven();
 Toaster toaster = new Toaster();
-
+//i added the user line so they interface would use their name in the title - if this doesn't work you can just take it out
+String user = (String) JOptionPane.showInputDialog(null, "My name is JARVIS, what's yours?", "Hello!", JOptionPane.QUESTION_MESSAGE, null, null);
 String[] options = {"Lights", "Door", "Camera", "Spotify", "Oven", "Toaster"};
 
-String choice = (String) JOptionPane.showInputDialog(null, "How can I help you", "Hello!", JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
+String choice = (String) JOptionPane.showInputDialog(null, "What can I help you with", "Hello!" + user, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
 
 if (choice.equalsIgnoreCase("lights")){
 	String[] v = {"Turn the lights On", "Turn the lights Off"};
 
-	String power = (String) JOptionPane.showInputDialog(null, "What would you like to do", "Lights", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
+	String power = (String) JOptionPane.showInputDialog(null, "What can I help you with", user+ "'s Lights", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
 
 	camera.camPower(power);
 	
@@ -27,7 +29,7 @@ if (choice.equalsIgnoreCase("lights")){
 if (choice.equalsIgnoreCase("door")){
 	String[] u = {"On", "Off"};
 
-	String power = (String) JOptionPane.showInputDialog(null, "What would you like to do", "Door", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
+	String power = (String) JOptionPane.showInputDialog(null, "What can I help you with", user+ "'s Door", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
 
 	camera.camPower(power);
 	
@@ -36,15 +38,23 @@ if (choice.equalsIgnoreCase("door")){
 if (choice.equalsIgnoreCase("camera")){
 	String[] a = {"Turn a camera On", "Turn a camera Off"};
 
-	String power = (String) JOptionPane.showInputDialog(null, "What would you like to do", "Camera", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
+	String power = (String) JOptionPane.showInputDialog(null, "What would you like to do", user+ "'s Camera", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
 
 	camera.camPower(power);
 	
 }
 if (choice.equalsIgnoreCase("spotify")){
-	String[] x = {"Genres", "Artists", "Songs", "Playlists", "All"};
+	String[] x = {"Genres", "Artists", "Songs", "Playlists"};
 
-	String power = (String) JOptionPane.showInputDialog(null, "What would you like to search", "Spotify", JOptionPane.QUESTION_MESSAGE, null, x, x[1]);
+	String search = (String) JOptionPane.showInputDialog(null, "What would you like to search", user+"'s Spotify", JOptionPane.QUESTION_MESSAGE, null, x, x[1]);
+
+	spotify.doSearch(search);
+	
+}
+if (choice.equalsIgnoreCase("thermostat"){
+	String[] x = {"Heating", "Cooling", "Turn off Thermostat"};
+
+	String power = (String) JOptionPane.showInputDialog(null, "What can I help you with", user "'s Thermostat", JOptionPane.QUESTION_MESSAGE, null, x, x[1]);
 
 	camera.camPower(power);
 	
@@ -52,7 +62,7 @@ if (choice.equalsIgnoreCase("spotify")){
 if (choice.equalsIgnoreCase("oven")){
 	String[] y = {"Turn the oven On", "Turn the oven Off"};
 
-	String power = (String) JOptionPane.showInputDialog(null, "What would you like to do", "Oven", JOptionPane.QUESTION_MESSAGE, null, y, y[1]);
+	String power = (String) JOptionPane.showInputDialog(null, "What can I help you with", user+ "'s Oven", JOptionPane.QUESTION_MESSAGE, null, y, y[1]);
 
 	oven.ovenPower(power);
 	
@@ -60,7 +70,7 @@ if (choice.equalsIgnoreCase("oven")){
 if (choice.equalsIgnoreCase("toaster")){
 	String[] z = {"On", "Off"};
 
-	String power = (String) JOptionPane.showInputDialog(null, "What would you like to do", "Toaster", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
+	String power = (String) JOptionPane.showInputDialog(null, "What can I help you with", user+"'s Toaster", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
 
 	camera.camPower(power);
 	
