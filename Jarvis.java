b3de61da27697c750006849be613e2dce3cf2192
quinones17 <pquinones1@student.gsu.edu>
2017-04-30 14:45:47 -0400

    Add files via upload

--- Jarvis.java
+++ Jarvis.java
@@ -10,7 +10,48 @@ String[] options = {"Lights", "Door", "Camera", "Spotify", "Oven", "Toaster"};
 
 String choice = (String) JOptionPane.showInputDialog(null, "What would you like to use", "Hello!", JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
 
+if (choice.equalsIgnoreCase("lights")){
+	String[] a = {"On", "Off"};
+
+	String power = (String) JOptionPane.showInputDialog(null, "What would you like to do", "Lights", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
+
+	camera.camPower(power);
+	
+}
+if (choice.equalsIgnoreCase("door")){
+	String[] a = {"On", "Off"};
+
+	String power = (String) JOptionPane.showInputDialog(null, "What would you like to do", "Door", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
+
+	camera.camPower(power);
+	
+}
+
 if (choice.equalsIgnoreCase("camera")){
+	String[] a = {"On", "Off"};
+
+	String power = (String) JOptionPane.showInputDialog(null, "What would you like to do", "Camera", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
+
+	camera.camPower(power);
+	
+}
+if (choice.equalsIgnoreCase("spotify")){
+	String[] a = {"On", "Off"};
+
+	String power = (String) JOptionPane.showInputDialog(null, "What would you like to do", "Camera", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
+
+	camera.camPower(power);
+	
+}
+if (choice.equalsIgnoreCase("oven")){
+	String[] a = {"On", "Off"};
+
+	String power = (String) JOptionPane.showInputDialog(null, "What would you like to do", "Camera", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
+
+	camera.camPower(power);
+	
+}
+if (choice.equalsIgnoreCase("toaster")){
 	String[] a = {"On", "Off"};
 
 	String power = (String) JOptionPane.showInputDialog(null, "What would you like to do", "Camera", JOptionPane.QUESTION_MESSAGE, null, a, a[1]);
