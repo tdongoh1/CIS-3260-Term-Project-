166765a63a9dab08f0ba23a1e3684fb5a9539ac0
Roshini Jagadam <roshinijj@yahoo.com>
2017-04-30 17:33:34 -0400

    Update Oven.java

--- Oven.java
+++ Oven.java
@@ -7,24 +7,26 @@ public Scanner scan = new Scanner(System.in);
 
 public  void ovenPower(String power){
 	if(power.equalsIgnoreCase("on")){
-		System.out.println("Please select a temperature setting (200-450°F");
-		int temp = scan.nextInt();
+			
+	String t = (String)JOptionPane.showInputDialog(null,"Please select a temperature setting (200-450°F)", 
+	"Oven", JOptionPane.PLAIN_MESSAGE);
+
+	ovenTemp(t);
+
 	}
 	else if(power.equalsIgnoreCase("off")){
-		System.out.println("Oven is off");
+	   JOptionPane.showMessageDialog(null, "Oven is off", "Oven");
 	}
-	else{
-		System.out.println("Invalid input!");
-	}
+	
 }
 
 public void ovenTemp(int temp){
 if(temp>200&& temp<450){
-	System.out.println("Oven has been set to " + temp +"°F");
+		JOptionPane.showMessageDialog(null, "Oven has been set to " + temp +"°F", "Oven");
 	
 }
 else{
-	System.out.println("Temperature setting must be 200-450°F!");
+		JOptionPane.showMessageDialog(null, "Temperature setting must be 200-450°F!", "Oven", JOptionPane.ERROR_MESSAGE);
 }
 }}
 
