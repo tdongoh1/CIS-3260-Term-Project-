6bdffa2ced0049a1ea5672808b37df4c4b399f29
Roshini Jagadam <roshinijj@yahoo.com>
2017-04-30 12:49:46 -0400

    Update Spotify.java

--- Spotify.java
+++ Spotify.java
@@ -3,30 +3,33 @@
 
 	public class Spotify {
 		private Scanner scan = new Scanner(System.in);
-	public void genreName(String genre){
-		if(genre.equalsIgnoreCase("pop")){
+	
+	public void genreSearch(String genre){
+		if(genre.equalsIgnoreCase("pop"))
+		{
 			System.out.println("Now playing pop station!");
 		}
-		else if(genre.equalsIgnoreCase("rock")){
+		else if(genre.equalsIgnoreCase("rock"))
+		{
 				System.out.println("Now playing rock station!");
 		}
-		else if(genre.equalsIgnoreCase("country")){
+		else if(genre.equalsIgnoreCase("country"))
+		{
 			System.out.println("Now playing country station!");
 		}
-		else if(genre.equalsIgnoreCase("alternative")){
+		else if(genre.equalsIgnoreCase("alternative"))
+		{
 			System.out.println("Now playing alternative station!!");
 		}
-		else if(genre.equalsIgnoreCase("rap")){
+		else if(genre.equalsIgnoreCase("rap"))
+		{
 			System.out.println("Now playing rap station!");
 		}
-		else {
+		else 
+		{
 		System.out.println("I could not find " +genre +" on Spotify!");
-		
-	}
-
-
-		
 		}
 
-
+	
+		}
 	}
