77b753326c04be32f725a6408cc740accd417047
Roshini Jagadam <roshinijj@yahoo.com>
2017-04-30 22:18:37 -0400

    Update Camera.java

--- Camera.java
+++ Camera.java
@@ -7,17 +7,17 @@ public class Camera{
 private Scanner scan = new Scanner(System.in);
 
 	public void camPower(String power){
-		if(power.equalsIgnoreCase("on")){
-			String[] b = {"Front Door", "Back Door", "Foyer", "Kitchen", "Garage"};
+		if(power.equalsIgnoreCase("turn a camera on")){
+			String[] b = {"All cameras","Front Door camera", "Back Door camera", "Foyer camera", "Kitchen camera", "Garage camera"};
 
 			String camPower = (String) JOptionPane.showInputDialog(null, "Which camera should I turn on", "Camera", JOptionPane.QUESTION_MESSAGE, null, b, b[1]);
 
 			camOn(camPower);
 			
 		}
-		else if(power.equalsIgnoreCase("off")){
+		else if(power.equalsIgnoreCase("turn a camera off")){
 			
-			String[] b = {"Front Door", "Back Door", "Foyer", "Kitchen", "Garage"};
+			String[] b = {"All cameras", "Front Door camera", "Back Door camera", "Foyer camera", "Kitchen camera", "Garage camera"};
 
 			String camPowerOff = (String) JOptionPane.showInputDialog(null, "Which camera should I turn off", "Camera", JOptionPane.QUESTION_MESSAGE, null, b, b[1]);
 			
@@ -29,12 +29,12 @@ private Scanner scan = new Scanner(System.in);
 	
 	public void camOn(String whichCam){
 	
-			System.out.println(whichCam + " is now on!" );
+			System.out.println(whichCam + " now on!" );
 		}
 	
 	public void camOff(String whichCamOff){
 		
-				System.out.println(whichCamOff + " is now off!");
+				System.out.println(whichCamOff + " now off!");
 	}
 }
 
