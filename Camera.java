6c4209fff355b741710f3b910fb4a11a9d01fc1e
Roshini Jagadam <roshinijj@yahoo.com>
2017-04-30 22:48:56 -0400

    Update Camera.java

--- Camera.java
+++ Camera.java
@@ -10,31 +10,31 @@ private Scanner scan = new Scanner(System.in);
 		if(power.equalsIgnoreCase("turn a camera on")){
 			String[] b = {"All cameras","Front Door camera", "Back Door camera", "Foyer camera", "Kitchen camera", "Garage camera"};
 
-			String camPower = (String) JOptionPane.showInputDialog(null, "Which camera should I turn on", "Camera", JOptionPane.QUESTION_MESSAGE, null, b, b[1]);
+			String whichCam = (String) JOptionPane.showInputDialog(null, "Which camera should I turn on", "Camera", JOptionPane.QUESTION_MESSAGE, null, b, b[1]);
 
-			camOn(camPower);
+			camOn(whichCam);
 			
 		}
 		else if(power.equalsIgnoreCase("turn a camera off")){
 			
 			String[] b = {"All cameras", "Front Door camera", "Back Door camera", "Foyer camera", "Kitchen camera", "Garage camera"};
 
-			String camPowerOff = (String) JOptionPane.showInputDialog(null, "Which camera should I turn off", "Camera", JOptionPane.QUESTION_MESSAGE, null, b, b[1]);
+			String whichCamOff = (String) JOptionPane.showInputDialog(null, "Which camera should I turn off", "Camera", JOptionPane.QUESTION_MESSAGE, null, b, b[1]);
 			
 	
-			camOff(camPowerOff);	
+			camOff(whichCamOff);	
 		}
 	 
 		}
 	
 	public void camOn(String whichCam){
 	
-			JOptionPane.showMessageDialog(null, whichCam + " now on!", "Camera" );
+			JOptionPane.showMessageDialog(null, whichCam + " now on!", "Camera", null );
 		}
 	
 	public void camOff(String whichCamOff){
 		
-			JOptionPane.showMessageDialog(null, whichCamOff + " now off!", "Camera" );
+			JOptionPane.showMessageDialog(null, whichCamOff + " now off!", "Camera", null);
 	}
 }
 
